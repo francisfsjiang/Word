eb0f432eadc4471a8ad8e2ea147b128b5b89d812
neveralso <287459525@qq.com>
2013-04-04 14:06:16 +0800

    0.11

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -41,36 +41,7 @@ namespace Word
             this.Frame.Navigate(typeof(AddPage));
         }
 
-        public class Customer
-        {
-            public String FirstName { get; set; }
-            public String LastName { get; set; }
-            public String Address { get; set; }
 
-            public Customer(String firstName, String lastName, String address)
-            {
-                this.FirstName = firstName;
-                this.LastName = lastName;
-                this.Address = address;
-            }
-
-        }
-
-        public class Customers : ObservableCollection<Customer>
-        {
-            public Customers()
-            {
-                Add(new Customer("Michael", "Anderberg",
-                        "12 North Third Street, Apartment 45"));
-                Add(new Customer("Chris", "Ashton",
-                        "34 West Fifth Street, Apartment 67"));
-                Add(new Customer("Cassie", "Hicks",
-                        "56 East Seventh Street, Apartment 89"));
-                Add(new Customer("Guido", "Pica",
-                        "78 South Ninth Street, Apartment 10"));
-            }
-
-        }
 
 
     }
